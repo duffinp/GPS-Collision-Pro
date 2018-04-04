6c9431b217b3773ad0b73cf9f79a0a47fccd28ae
Paul Duffin <pauldu@stud.ntnu.no>
2018-04-04 15:49:24 +0200

    Day12
    Added User Location and added warning label. We still need to make the warning label conditional and implement the GeoJSON file.

--- Collision Project/ViewController.swift
+++ Collision Project/ViewController.swift
@@ -8,18 +8,50 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var Warning: UILabel!
+    
+    var movedToUserLocation = false
+    
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let initial_Location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(63.415, 10.406)
-        //let initial_Location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(64.102031, -21.776083)
-        let distanceSpan:CLLocationDegrees = 2000
-        mapView.setRegion(MKCoordinateRegionMakeWithDistance(initial_Location, distanceSpan, distanceSpan), animated: true)
+        //let initial_Location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(63.415, 10.406)
+        
+                mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+                self.mapView.showsUserLocation = true
+        
     }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .denied:
+            print("We need user location.")
+        case .restricted:
+            print("User location is restricted. Change settings")
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default:
+            manager.startUpdatingLocation()
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        if !movedToUserLocation {
+            mapView.region.center = mapView.userLocation.coordinate
+            let distanceSpan:CLLocationDegrees = 2000
+            mapView.setRegion(MKCoordinateRegionMakeWithDistance(mapView.region.center, distanceSpan, distanceSpan), animated: true)
+            movedToUserLocation = true
+        }
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
