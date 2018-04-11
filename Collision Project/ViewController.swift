e525ea7d3cbd71c1c956bb38a14b065b56e10e7b
Paul Duffin <pauldu@stud.ntnu.no>
2018-04-11 16:48:30 +0200

    Day13
    Added functionality to call data from back-end server. Displays speed in user interface. Displays warning depending on the minimum depth given by the server.

--- Collision Project/ViewController.swift
+++ Collision Project/ViewController.swift
@@ -14,19 +14,23 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var Warning: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
     
     var movedToUserLocation = false
     
     let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
         //let initial_Location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(63.415, 10.406)
         
-                mapView.delegate = self
+            mapView.delegate = self
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
-                self.mapView.showsUserLocation = true
+            self.mapView.showsUserLocation = true
+        
+        
+        
         
     }
     
@@ -40,7 +44,70 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
             manager.requestWhenInUseAuthorization()
         default:
             manager.startUpdatingLocation()
+            manager.startUpdatingHeading()
         }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = locations[0]
+        
+        let span = MKCoordinateSpanMake(0.03, 0.03)
+        let myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        let region = MKCoordinateRegionMake(myLocation, span)
+        let depthBound = 2.0 as NSNumber
+        
+        
+        let testlat = 63.4313892
+        let testlng = 10.4050155
+        
+        let offlat = testlat - location.coordinate.latitude
+        let offlng = testlng - location.coordinate.longitude
+        
+        mapView.setRegion(region, animated: true)
+        self.mapView.showsUserLocation = true
+        
+        let strtest = "http://188.166.59.108:3000/api/v1/test?lat=\(location.coordinate.latitude + offlat)&lng=\(location.coordinate.longitude + offlng)&vel=100&bearing=0"
+        //let strtest = "http://188.166.59.108:3000/api/v1/test?lat=\(testlat)&lng=\(testlng)&vel=10&bearing=0"
+        let url = URL(string: strtest)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil     {
+                print("WARNING!")
+            }
+            else {
+                if let content = data   {
+                    do     {
+                        let myJson = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        print(myJson)
+                        
+                        if let minima = myJson["min"] as? NSNumber   {
+                            if (minima.floatValue < depthBound.floatValue) {
+                                DispatchQueue.main.async( execute: {
+                                    self.Warning.text = "WARNING!!!"
+                                })
+                            }
+                            else {
+                                DispatchQueue.main.async( execute: {
+                                    self.Warning.text = "Safe"
+                                })
+                            }
+                        }
+                        if let maxima = myJson["max"] as? NSNumber   {
+                            print("\(maxima)")
+                            
+                        }
+                        
+                    }
+                    catch {print("Error")}
+                }
+            }
+        }
+        task.resume()
+        
+        speedLabel.text = "Speed = \(location.speed.rounded()) m/s"
+        //let val = myJson
+        //speedLabel.text = "\(val)"
+        
     }
     
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
